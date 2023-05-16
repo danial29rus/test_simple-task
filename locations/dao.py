46de2b242797a2b945c439b54a8646e00fcdfe5b
Daniil <70702619+danial29rus@users.noreply.github.com>
2023-05-16 15:54:45 +0300

    create api_call

--- locations/dao.py
+++ locations/dao.py
@@ -2,9 +2,19 @@ import time
 import random
 from datetime import datetime, timedelta
 from typing import List
-from benchmark import timing
+import time
 
-@timing
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Время работы: {execution_time} секунды")
+        return result
+    return wrapper
+
+@measure_time
 def get_current_dislocation() -> List:
     """
     Формирование текущей дислокации вагонов. 
@@ -16,7 +26,7 @@ def get_current_dislocation() -> List:
     arrivale_dates = [None, None, None, datetime.now() - timedelta(days=3), datetime.now()]
     time.sleep(2)
 
-    for i in range(0, 20000):
+    for i in range(0, 10):
         arrivale_date = random.choice(arrivale_dates)
         location = {
             "wagon": random.randint(10000, 90000),
@@ -26,7 +36,7 @@ def get_current_dislocation() -> List:
         locations.append(location)    
     return locations
 
-@timing
+@measure_time
 def get_predicted_date_by_invoices(invoices: List) -> List:
     """
     На вход необходимо передать список из уникальных накладных. 
@@ -44,7 +54,7 @@ def get_predicted_date_by_invoices(invoices: List) -> List:
     
     return predicted_results
 
-@timing
+@measure_time
 def api_call():
     """
     В качестве ответа должен выдаваться повагонный список из сервиса get_current_dislocation 
@@ -52,13 +62,27 @@ def api_call():
     только по вагоном, у которых она отсутствует
     """
     locations = get_current_dislocation()
+
     # Получить список уникальных накладных из текущей дислокации только по тем вагонам,
     # где arrivale_date = None
-    invoices = []
+    invoices = [location['invoice'] for location in locations if location['arrivale_date'] is None]
+
     predicted_data = get_predicted_date_by_invoices(invoices)
 
     # Обновить оригинальный список вагонов данными, которые прислал сервис get_predicted_dates().
-    # Заменить вагоны, где arrivale_date = None на соответствующее поле predicted_date.    
+    # Заменить вагоны, где arrivale_date = None на соответствующее поле predicted_date.
+
+    for location in locations:
+        if location['arrivale_date'] is None:
+            invoice = location['invoice']
+            predicted_dates = [data['predicted_date'] for data in predicted_data if data['invoice'] == invoice]
+            if predicted_dates:
+                location['arrivale_date'] = predicted_dates[0]
+
+    # Вернуть обновленный список вагонов
+    return locations
+
+print(api_call())
 
     
 
